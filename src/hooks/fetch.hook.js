5c6d13dd5580b93bb968361ef98b5d0cb38244ef
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2024-01-06 10:45:54 +0530

    Update fetch.hook.js

--- src/hooks/fetch.hook.js
+++ src/hooks/fetch.hook.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { getUserName } from "../helper/helper";
-axios.defaults.baseURL = process.env.LINK
+axios.defaults.baseURL = `https://auth-server-zeta.vercel.app`
 
 export const useFetch=(query)=>{
     const [getData, setData] = useState({
